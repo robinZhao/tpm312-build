a9d6877067e87cfd46351a076e86bf1586d51d63
赵锐斌 <zhaoruibin@dcm360.cn>
2024-08-20 12:37:52 +0800

    修改脚本

--- mkdtb.sh
+++ mkdtb.sh
@@ -4,9 +4,8 @@ build_dtb(){
 	local dtbfile=$2;
 	scp $dtbfile.dts root@192.168.20.190:/opt/$linuxdir/arch/arm64/boot/dts/rockchip/$dtbfile.dts
 	ssh -t root@192.168.20.190 "cd /opt/$linuxdir/;  source /etc/profile; make rockchip/$dtbfile.dtb"
-	scp root@192.168.20.190:/opt/$linuxdir/arch/arm64/boot/dts/rockchip/$dtbfile.dtb ./$dtbfile-$linuxdir.dtb
+	scp root@192.168.20.190:/opt/$linuxdir/arch/arm64/boot/dts/rockchip/$dtbfile.dtb ./$3.dtb
 }
 
-build_dtb linux-6.6.45 ophub-tpm312
-build_dtb linux-6.9 ophub-tpm312
-build_dtb linux-6.6 ophub-tpm312
+build_dtb $1 $2 $3
+# ./mkdtb linux-6.6 ophub-tpm312 rk3399-tpm312
