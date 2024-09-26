6e0a4ba14c4d7c0ea7cc612e1d295411cbb8167c
Robin <zhaoruibin@aliyun.com>
2024-09-27 03:16:02 +0800

    Update tpm312-firstrun.sh

--- userpatches/extensions/tpm312-firstrun.sh
+++ userpatches/extensions/tpm312-firstrun.sh
@@ -1,12 +1,12 @@
 function post_family_tweaks__preset_configs() {
 	display_alert "$BOARD" "preset configs for rootfs" "info"
 	# Set PRESET_NET_CHANGE_DEFAULTS to 1 to apply any network related settings below
-	echo "PRESET_NET_CHANGE_DEFAULTS=1" > "${SDCARD}"/root/.not_logged_in_yet
+	#echo "PRESET_NET_CHANGE_DEFAULTS=1" > "${SDCARD}"/root/.not_logged_in_yet
 
 	# Enable WiFi or Ethernet.
 	#      NB: If both are enabled, WiFi will take priority and Ethernet will be disabled.
-	echo "PRESET_NET_ETHERNET_ENABLED=1" >> "${SDCARD}"/root/.not_logged_in_yet
-	echo "PRESET_NET_WIFI_ENABLED=1" >> "${SDCARD}"/root/.not_logged_in_yet
+	#echo "PRESET_NET_ETHERNET_ENABLED=1" >> "${SDCARD}"/root/.not_logged_in_yet
+	#echo "PRESET_NET_WIFI_ENABLED=1" >> "${SDCARD}"/root/.not_logged_in_yet
 
 	#Enter your WiFi creds
 	#      SECURITY WARN: Your wifi keys will be stored in plaintext, no encryption.
@@ -14,7 +14,7 @@ function post_family_tweaks__preset_configs() {
 	#echo "PRESET_NET_WIFI_KEY='MyWiFiKEY'" >> "${SDCARD}"/root/.not_logged_in_yet
 
 	#      Country code to enable power ratings and channels for your country. eg: GB US DE | https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
-	echo "PRESET_NET_WIFI_COUNTRYCODE='GB'" >> "${SDCARD}"/root/.not_logged_in_yet
+	#echo "PRESET_NET_WIFI_COUNTRYCODE='GB'" >> "${SDCARD}"/root/.not_logged_in_yet
 
 	#If you want to use a static ip, set it here
 	#echo "PRESET_NET_USE_STATIC=1" >> "${SDCARD}"/root/.not_logged_in_yet
