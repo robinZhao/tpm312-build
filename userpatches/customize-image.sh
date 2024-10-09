6fb15f48c687e34a116dfdaadd6484e9cd3ff860
Robin <zhaoruibin@aliyun.com>
2024-10-09 22:48:50 +0800

    Update customize-image.sh

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -36,30 +36,36 @@ Main() {
 			# your code here
 			;;
                 jammy)
-			if [[ "${BUILD_DESKTOP}" == "yes" ]];then
-		            mv "${SDCARD}"/etc/apt/sources.list.d/armbian.list.disabled  "${SDCARD}"/etc/apt/sources.list.d/armbian.list
-                            apt-get update
-			    if [[ "${ENABLE_EXTENSIONS}" =~ "mesa-vpu" ]];then
-       			       echo "mesa-vpu contains chromium"
-			    else
-       		               apt-get install -y chromium
-       			    fi
-			    apt-get install -y fcitx5 fcitx5-chinese-addons fcitx5-frontend-gtk4 fcitx5-frontend-gtk3 fcitx5-frontend-gtk2 \
-                            fcitx5-frontend-qt5 fcitx5-config-qt fcitx5-modules 
-			    cp --parents /usr/share/applications/org.fcitx.Fcitx5.desktop /etc/xdg/autostart/
-			    if [[ "${DESKTOP_ENVIRONMENT}" == "gnome" ]];then
-			        apt-get install -y gnome-tweaks  gnome-shell-extension-prefs  chrome-gnome-shell gnome-shell-extension-manager
-   			    fi
-			    mv "${SDCARD}"/etc/apt/sources.list.d/armbian.list  "${SDCARD}"/etc/apt/sources.list.d/armbian.list.disabled
-                        fi
-			apt-get install -y vim;
-			systemctl mask hibernate.target
-			systemctl mask suspend.target
-                        
+			PostProcess                        
 	                ;;
+		noble)
+  			PostProcess
+   			;;
 	esac
 } # Main
 
+PostProcess()
+{
+	if [[ "${BUILD_DESKTOP}" == "yes" ]];then
+	    mv "${SDCARD}"/etc/apt/sources.list.d/armbian.list.disabled  "${SDCARD}"/etc/apt/sources.list.d/armbian.list
+	    apt-get update
+	    if [[ "${ENABLE_EXTENSIONS}" =~ "mesa-vpu" ]];then
+	       echo "mesa-vpu contains chromium"
+	    else
+	       apt-get install -y chromium
+	    fi
+	    apt-get install -y fcitx5 fcitx5-chinese-addons fcitx5-frontend-gtk4 fcitx5-frontend-gtk3 fcitx5-frontend-gtk2 \
+	    fcitx5-frontend-qt5 fcitx5-config-qt fcitx5-modules 
+	    cp --parents /usr/share/applications/org.fcitx.Fcitx5.desktop /etc/xdg/autostart/
+	    if [[ "${DESKTOP_ENVIRONMENT}" == "gnome" ]];then
+		apt-get install -y gnome-tweaks  gnome-shell-extension-prefs  chrome-gnome-shell gnome-shell-extension-manager
+	    fi
+	    mv "${SDCARD}"/etc/apt/sources.list.d/armbian.list  "${SDCARD}"/etc/apt/sources.list.d/armbian.list.disabled
+	fi
+	apt-get install -y vim;
+	systemctl mask hibernate.target
+	systemctl mask suspend.target
+}
 
 InstallAdvancedDesktop()
 {
