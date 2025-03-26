c8432f5a23785c5a553fcb9a83b918bcc8c95744
Robin <zhaoruibin@aliyun.com>
2025-03-26 09:18:44 +0800

    Update customize-image.sh

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -58,6 +58,7 @@ PostProcess()
 	    mv "${SDCARD}"/etc/apt/sources.list.d/armbian.list  "${SDCARD}"/etc/apt/sources.list.d/armbian.list.disabled
 	fi
 	apt-get install -y vim
+ 	apt-get clean
 	systemctl mask hibernate.target
 	systemctl mask suspend.target
 }
